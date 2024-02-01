d95ee85a8dc00b12970cc14b38548563ff5e2e15
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-02-01 17:20:45 +0100

    Initial commit

---
+++ content-calendar/src/main/groovy/dev/fcosta/contentcalendar/ContentCalendarApplication.groovy
@@ -0,0 +1,13 @@
+package dev.fcosta.contentcalendar
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ContentCalendarApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ContentCalendarApplication, args)
+	}
+
+}
