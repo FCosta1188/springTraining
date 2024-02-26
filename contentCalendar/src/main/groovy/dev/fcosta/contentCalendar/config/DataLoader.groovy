12ec06d40d52fe55229b8e19fabab02f364dc709
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-02-26 17:45:09 +0100

    Added springToProduction.md and related docker file

--- contentCalendar/src/main/groovy/dev/fcosta/contentCalendar/config/DataLoader.groovy
+++ contentCalendar/src/main/groovy/dev/fcosta/contentCalendar/config/DataLoader.groovy
@@ -25,10 +25,10 @@ class DataLoader implements CommandLineRunner { // CommandLineRunner:  interface
         println "----------------"*/
 
         // init the repo with the contents of ./resources/data/content.json
-        if(repository.count()) {
+        //if(!repository.count()) { // init with these dummy data only if the db is empty (repository.count() == 0)
             try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
                 repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>(){}))
             }
-        }
+        //}
     }
 }
