8e7f8801b0becc904ac23b9428f254ce56f366af
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-04-04 17:13:49 +0200

    Added CorsFilter bean in Application

--- contentCalendar/src/main/groovy/dev/fcosta/contentCalendar/Application.groovy
+++ contentCalendar/src/main/groovy/dev/fcosta/contentCalendar/Application.groovy
@@ -4,7 +4,11 @@ import dev.fcosta.contentCalendar.config.ContentCalendarProperties
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 // ***** https://www.youtube.com/watch?v=UgX5lgv4uVM *****
 
@@ -42,8 +46,8 @@ class Application {
         //println rt
     }
 
-    // Data init: see below or DataLoader class
 /*    @Bean
+    // Data initialization: see below or DataLoader class
     CommandLineRunner commandLineRunner(ContentRepository repository) {
         return (args) -> {
             Content content = new Content(
@@ -60,4 +64,43 @@ class Application {
             repository.save(content)
         }
     }*/
+
+    @Bean
+    //Allows communication between apps located in different domains (cross-origin), eg backend and frontend
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource()
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration()
+        corsConfiguration.setAllowCredentials(true)
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:4200"
+        ))
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "Origin",
+                "Access-Control-Allow-Origin",
+                "Content-Type",
+                "Accept",
+                "Jwt-Token",
+                "Authorization",
+                "Origin, Accept",
+                "X-Requested-With",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers",
+        ))
+        corsConfiguration.setExposedHeaders(Arrays.asList(
+                "Origin",
+                "Content-Type",
+                "Accept",
+                "Jwt-Token",
+                "Authorization",
+                "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials",
+                "Filename"
+        ))
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"))
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+        return new CorsFilter(urlBasedCorsConfigurationSource)
+    }
 }
